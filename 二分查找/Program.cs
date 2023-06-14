2671e0585cf32a2ac426f7d74b281652f982066c
胡梦建 <2351491149@qq.com>
2023-06-14 13:35:30 +0800

    1

---
+++ 二分查找/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace 二分查找
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] s = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 };
+        }
+    }
+}
---
+++ 最小路径和/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace 最小路径和
+{
+    internal class Program
+    {
+        /// <summary>
+        /// 最小路径和  动态规划
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            int[][] array = new int[2][];
+            array[0] = new int[] { 1, 2, 3 };
+            array[1] = new int[] { 4, 5, 6 };
+          
+          int i=  Solution. MinPathSum(array);
+            Console.WriteLine(  i.ToString());
+
+        }
+    }
+    public class Solution
+    {
+        public static int MinPathSum(int[][] grid)
+        {
+            int width = grid[0].Length;//宽
+            int high = grid.Length;//高
+            if (high == 0 || width == 0) return 0;
+            // 初始化
+            for (int i = 1; i < high; i++) grid[i][0] += grid[i - 1][0];
+            for (int i = 1; i < width; i++) grid[0][i] += grid[0][i - 1];
+            for (int i = 1; i < high; i++)
+            {
+                for (int j = 1; j < width; j++)
+                {
+                    grid[i][j] += Math.Min(grid[i - 1][j], grid[i][j - 1]);
+                }
+            }
+            return grid[high - 1][width - 1];
+        }
+    }
+}
